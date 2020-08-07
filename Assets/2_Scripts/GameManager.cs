affe84d7587b5deea15284c79491943d9e33765a
jaehanim1@gmail.com <idjaeha@JaehaLaptop.localdomain>
2020-08-07 19:22:10 +0900

    텍스트 파일을 이용한 대화 기능 구현

--- Assets/2_Scripts/GameManager.cs
+++ Assets/2_Scripts/GameManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    private MessageManager messageManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PostMessages("Assets\\3_Sources\\2_Dialogue\\Intro.txt");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void PostMessages(string filePath)
+    {
+        TextLoader textLoader = new TextLoader();
+        List<string> newMessages = textLoader.LoadText(filePath);
+        messageManager.ReceiveMessages(newMessages);
+    }
+
+
 }
---
+++ Assets/2_Scripts/TextLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class TextLoader : MonoBehaviour
+{
+    public List<string> LoadText(string filePath)
+    {
+        List<string> texts = new List<string>();
+        foreach (string text in File.ReadLines(filePath))
+        {
+            texts.Add(text);
+        }
+        return texts;
+    }
+}
