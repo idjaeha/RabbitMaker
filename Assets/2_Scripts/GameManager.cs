2ac0f816cfe94167ae58cb2bf4c5af8de80d244b
MunEunsil <muneunsil1220@gmail.com>
2020-08-10 22:00:23 +0900

    audioManager생성

--- Assets/2_Scripts/GameManager.cs
+++ Assets/2_Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField]
     private MessageManager messageManager;
-
+ 
     // Start is called before the first frame update
     void Start()
     {
